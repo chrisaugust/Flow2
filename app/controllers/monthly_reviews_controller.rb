8641474e87cc218d91d81ee13256c82e275167ca
Chris Danielson <christopher.a.danielson@protonmail.com>
2025-09-16 09:25:50 -0400

    Clean up monthly reviews controller

--- app/controllers/monthly_reviews_controller.rb
+++ app/controllers/monthly_reviews_controller.rb
@@ -21,23 +21,6 @@ class MonthlyReviewsController < ApplicationController
     render json: review, include: [:monthly_category_reviews, :user]
   end
 
-  # def create
-  #   date = params[:month].present? ? Date.parse(params[:month]) : Date.today
-  #   month_code = date.beginning_of_month.strftime('%m%Y')
-  #   review = @current_user.monthly_reviews.find_or_initialize_by(month_code: month_code)
-  #   if review.persisted?
-  #     render json: review, include: :monthly_category_reviews, status: :ok
-  #   else
-  #     review = MonthlyReviewBuilder.new(@current_user, date).build_review
-  #     unless review.persisted?
-  #       Rails.logger.debug ">>> Review validation errors: #{review.errors.full_messages}"
-  #       render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
-  #       return
-  #     end
-  #     render json: review, include: :monthly_category_reviews, status: :created
-  #   end
-  # end
-
   def create
     date = params[:month].present? ? Date.parse(params[:month]) : Date.today
     month_code = date.beginning_of_month.strftime('%m%Y')
@@ -69,56 +52,6 @@ class MonthlyReviewsController < ApplicationController
       @review.update(completed: true)
     end
   end
-
-  # def by_month_code
-  #   date = Date.strptime(params[:month_code], "%m%Y")
-  #   month_code = date.beginning_of_month.strftime("%m%Y")
-
-  #   # find or build review
-  #   review = @current_user.monthly_reviews.find_by(month_code: month_code)
-
-  #   unless review
-  #     begin
-  #       review = MonthlyReviewBuilder.new(@current_user, date).build_review
-  #     rescue => e
-  #       Rails.logger.debug ">>> Builder failed: #{e.class} - #{e.message}"
-  #       render json: { error: e.message }, status: :unprocessable_entity
-  #       return
-  #     end
-  #   end
-
-  #   render json: review, include: [:monthly_category_reviews, :user]
-  # end
-
-  # def by_month_code
-  #   date = Date.strptime(params[:month_code], "%m%Y")
-  #   month_code = date.beginning_of_month.strftime("%m%Y")
-
-  #   Rails.logger.debug ">>> Current user ID: #{@current_user.id}"
-  #   Rails.logger.debug ">>> Looking for month_code: #{month_code}"
-
-  #   review = @current_user.monthly_reviews.find_by(month_code: month_code)
-  #   if review
-  #     Rails.logger.debug ">>> Found existing review ID: #{review.id}"
-  #   else
-  #     Rails.logger.debug ">>> No review found for month_code, building new one..."
-  #     begin
-  #       review = MonthlyReviewBuilder.new(@current_user, date).build_review
-  #     rescue => e
-  #       Rails.logger.debug ">>> Builder failed: #{e.class} - #{e.message}"
-  #       render json: { error: e.message }, status: :unprocessable_content
-  #       return
-  #     end
-
-  #     unless review.persisted?
-  #       Rails.logger.debug ">>> MonthlyReviewBuilder validation errors: #{review.errors.full_messages}"
-  #       render json: { errors: review.errors.full_messages }, status: :unprocessable_content
-  #       return
-  #     end
-  #   end
-
-  #   render json: review, include: [:monthly_category_reviews, :user]
-  # end
 
   def by_month_code
     review = @current_user.monthly_reviews.find_by!(month_code: params[:month_code])
