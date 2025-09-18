82457be1258b2af8fe83631cf5b1a36c5fc2615a
Chris Danielson <christopher.a.danielson@protonmail.com>
2025-09-18 11:26:32 -0400

    Configure smtp for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "flow2_api_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'flow2-frontend.vercel.app', 
+    user_name:            ENV['GMAIL_USERNAME'], 
+    password:             ENV['GMAIL_APP_PASSWORD'], 
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { host: 'flow2-frontend.vercel.app' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
